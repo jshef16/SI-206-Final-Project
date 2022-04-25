8f666fd712dd56c13e7060490a0fe14a7a76bc30
jshefman <jshefman@umich.edu>
2022-04-25 17:31:07 -0400

    final

--- bikes.py
+++ bikes.py
@@ -1,18 +1,35 @@
 from asyncore import write
 from itertools import count
 from turtle import color
+from bs4 import BeautifulSoup
 import unittest
 import sqlite3
 import json
+import re
 import os
 import requests
 import matplotlib.pyplot as plt
 import numpy as np
+from statistics import stdev
+from statistics import mean
+from statistics import median
 
 
 
-def readfile():
-    pass
+def get_data():
+    url = 'https://copenhagenizeindex.eu/'
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    city_list = []  
+    table = soup.find('div', class_ = 'items')
+    specific_cities = table.find_all('a', class_ = 'link')
+    for specific_city in specific_cities:
+        city_rank = specific_city.find('div', class_ = 'index19').text.strip()
+        city_name = specific_city.find('div', class_ = 'name colorize').text.strip()
+        city_score = specific_city.find('div', class_ = 'total-score total-score--sm').text.strip()
+        city_list.append((city_rank, city_name, city_score))
+    print(city_list)
+    return(city_list)
 
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -160,7 +177,7 @@ def writeData(counts):
 
     f.write("CITY,COUNTRY,NUMBER OF BIKES\n")
     for row in counts:
-        f.write(str(row[0]) + "," + str(row[1]) + "," + str(row[2]) + "\n")
+        f.write(str(row[0]).strip() + "," + str(row[1]).strip() + "," + str(row[2]).strip() + "\n")
 
     f.close()
 
@@ -190,7 +207,78 @@ def bikesByCompany(cur, conn):
     plt.tight_layout()
     plt.show()
 
+def city_lst(city_list):
+    cities = []
+    for item in city_list:
+        city = item[1]  
+        cities.append(city)
+    return(cities)
+
+def score_lst(city_list):
+    scores_string = ''
+    scores = []
+    for item in city_list:
+        score_perc = str(item[2])       
+        scores_string += score_perc
+    score_str_list = re.findall('\d+\.\d+', scores_string)
+    for score_ in score_str_list:
+        score = float(score_)
+        scores.append(score)
+    return(scores)
+
+def stats(scores):
+    stats = []
+    mean_value = round(mean(scores), 1)
+    median_value = round(median(scores), 1)
+    std = round(stdev(scores), 1)
+    scores_array = np.array(scores)
+    print("Mean of scores: ", mean_value)
+    print("Median of scores: ", median_value)
+    print("Standard Deviation of scores: ", std)
+    print("Q1 quantile of scores: ", round(np.quantile(scores_array, .25), 1))
+    print("Q2 quantile of scores: ", round(np.quantile(scores_array, .50), 1))
+    print("Q3 quantile of scores: ", round(np.quantile(scores_array, .75), 1))
+    print("100th quantile of scores: ", round(np.quantile(scores_array, 1), 1)) 
+    stats.append(mean_value)
+    stats.append(median_value)
+    stats.append(std)
+    stats.append(round(np.quantile(scores_array, .25), 1))
+    stats.append(round(np.quantile(scores_array, .50), 1))
+    stats.append(round(np.quantile(scores_array, .75), 1))
+    stats.append(round(np.quantile(scores_array, 1), 1))
+    print(stats)
+    return stats
+
+def city_score_visual(cities, scores):
+    plt.figure(1, figsize=(10, 10))
+    ax = plt.axes()
+    ax.set_facecolor('0')
+    plt.barh(cities, scores, color = 'grey')
+    plt.gca().invert_yaxis()
+    for index, value in enumerate(scores):
+        plt.text(value, index, str(value), color = 'w')
+    plt.suptitle('The Most Bicycle-Friendly Cities in 2019')
+    plt.xlabel('Score')
+    plt.ylabel('City')
+    plt.savefig('The Most Bicycle-Friendly Cities in 2019')
+    plt.show()
+
+def stats_visual(scores_stats):
+    Name = ['Mean', 'Median', 'Standard Deviation', 'Q1', 'Q2', 'Q3', 'Q4']
+    plt.figure(1, figsize=(10, 10))
+    ax = plt.axes()
+    ax.set_facecolor('0')
+    plt.bar(Name, scores_stats, color = 'grey')
+    for value, index in enumerate(scores_stats):
+        plt.text(value, index, str(index), color = 'w')
+    plt.suptitle('Statistics of the Copenhagenize Index')
+    plt.xlabel('Stats')
+    plt.ylabel('Scores')
+    plt.savefig('Statistics of the Copenhagenize Index')
+    plt.show()  
+
 def main():
+    get_data()
     # create database
     cur, conn = setUpDatabase('bikes.db')
     # set up tables
@@ -213,6 +301,12 @@ def main():
     max_cities = getMaxCount(counts)
     writeData(counts)
     bikesByCompany(cur, conn)
+    city_list = get_data()
+    cities = city_lst(city_list)
+    scores = score_lst(city_list)
+    scores_stats = stats(scores)
+    city_score_visual(cities, scores)
+    stats_visual(scores_stats)
 
 
 
